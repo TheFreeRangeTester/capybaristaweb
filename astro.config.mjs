c307a5a852332c9f4046c9dcd5b1befcfd36521b
Pato Miner <patominer@gmail.com>
2026-05-01 08:38:33 +1200

    Set Capybarista custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://capybarista.com",
+  site: "https://capybarista.studio",
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
